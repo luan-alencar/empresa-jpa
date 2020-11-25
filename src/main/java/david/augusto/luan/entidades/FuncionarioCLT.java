ee826193f6832da651c75ffc0561d8dfea923c21
luan-alencar <luanalencar134@gmail.com>
2020-11-25 12:31:01 -0300

    commit

---
+++ src/main/java/david/augusto/luan/entidades/FuncionarioCLT.java
@@ -0,0 +1,52 @@
+package david.augusto.luan.entidades;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "funcionario_clt")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class FuncionarioCLT extends Funcionario {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	@Column(name = "salario")
+	private Double salario;
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = super.hashCode();
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (!super.equals(obj))
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		FuncionarioCLT other = (FuncionarioCLT) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+}
